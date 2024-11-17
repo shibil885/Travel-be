d46184a4f73d52f5189f4327507a7432201948d1
shibil885 <shibilmuhammed885@gmail.com>
2024-11-17 15:02:59 +0530

    edit offer dto created

---
+++ src/common/dtos/editOffer.dto.ts
@@ -0,0 +1,34 @@
+import {
+  IsString,
+  IsEnum,
+  IsDate,
+  IsNumber,
+  IsOptional,
+} from 'class-validator';
+import { OfferType } from 'src/common/enum/offerType.enum';
+
+export class EditOfferDto {
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsEnum(OfferType)
+  @IsOptional()
+  discount_type?: OfferType;
+
+  @IsNumber()
+  @IsOptional()
+  discount_value?: number;
+
+  @IsNumber()
+  @IsOptional()
+  percentage?: number;
+
+  @IsDate()
+  @IsOptional()
+  expiry_date?: Date;
+}
