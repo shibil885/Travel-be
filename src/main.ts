7280ebc9075d7edeccb48f64ee7ee8ae61177f42
shibil885 <shibilmuhammed885@gmail.com>
2024-12-20 12:18:55 +0530

    enter to report management

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://travel.shibil.site',
+    // origin: 'https://travel.shibil.site',
+    origin: 'http://localhost:4200',
     credentials: true,
   });
   app.use(bodyParser.json());
