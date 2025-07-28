6d7ffcf7a416ab4b33e0cfd26712d9b28c3846c9
shibil885 <shibilmuhammed885@gmail.com>
2025-07-28 20:04:04 +0530

    feat: base repository implemented

---
+++ src/repositories/base.repository.ts
@@ -0,0 +1,25 @@
+import { Model, Document } from 'mongoose';
+
+export class BaseRepository<T extends Document> {
+  constructor(protected readonly model: Model<T>) {}
+
+  async findAll(): Promise<T[]> {
+    return this.model.find().exec();
+  }
+
+  async findById(id: string): Promise<T | null> {
+    return this.model.findById(id).exec();
+  }
+
+  async create(data: Partial<T>): Promise<T> {
+    return this.model.create(data);
+  }
+
+  async update(id: string, data: Partial<T>): Promise<T | null> {
+    return this.model.findByIdAndUpdate(id, data, { new: true }).exec();
+  }
+
+  async delete(id: string): Promise<T | null> {
+    return this.model.findByIdAndDelete(id).exec();
+  }
+}
